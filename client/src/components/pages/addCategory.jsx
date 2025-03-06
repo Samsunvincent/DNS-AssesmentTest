993bb5eae0c3069fc95773726ce02b64751ffc94
samsun <samsunvincent143@gmail.com>
2025-03-06 13:48:04 +0530

    deploy

--- client/src/components/pages/addCategory.jsx
+++ client/src/components/pages/addCategory.jsx
@@ -13,7 +13,7 @@ const AddCategoryForm = () => {
     const body = { name : category, description };
 
     try {
-      let response = await axios.post(`https://dns-assesmenttest-server.onrender.com/addCategory`, body, {
+      let response = await axios.post(`http://localhost:4000/addCategory`, body, {
         headers: { "Content-Type": "application/json" },
       });
       setMessage("Category added successfully!");
